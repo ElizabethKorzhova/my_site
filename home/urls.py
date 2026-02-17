b2644981ad949f2321365cabf5c0117d07b1e887
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2026-02-17 12:54:02 +0100

    chore: add type annotations to views and urls

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path, re_path
+"""URL configuration for home application in my_site project.
+The `urlpatterns` list routes URLs to corresponding views."""
+
+from django.urls import path, re_path, URLPattern
 from . import views
 
-urlpatterns = [
+urlpatterns: list[URLPattern] = [
     path("home/", views.home_view, name="home"),
     path("about/", views.about_view, name="about"),
     path("contact/", views.contact_view, name="contact"),
     re_path(r"^post/(?P<post_id>\d+)/$", views.post_view, name="post"),
     re_path(r"^profile/(?P<username>[A-za-z]+)/$", views.profile_view, name="profile"),
-    re_path(r"^event/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$", views.event_view, name="event"),
+    re_path(r"^event/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$", views.event_view,
+            name="event"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,31 +1,43 @@
+"""Views for the working with routing in home application.
+Contains function-based views responsible for rendering pages based on template."""
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 
-def home_view(request):
+def home_view(request: HttpRequest) -> HttpResponse:
+    """View function for home page."""
     return render(request, "home/index.html",
                   {"title": "home", "data": "Welcome to the home page"})
 
 
-def about_view(request):
+def about_view(request: HttpRequest) -> HttpResponse:
+    """View function for about page."""
     return render(request, "home/index.html",
                   {"title": "about", "data": "About page"})
 
 
-def contact_view(request):
+def contact_view(request: HttpRequest) -> HttpResponse:
+    """View function for contact page."""
     return render(request, "home/index.html",
                   {"title": "contact", "data": "Contact us"})
 
 
-def post_view(request, post_id):
+def post_view(request: HttpRequest, post_id: int) -> HttpResponse:
+    """View function for post page."""
     return render(request, "home/index.html",
-                  {"title": f"post {post_id}", "data": f"You are viewing the post with ID: {post_id}"})
+                  {"title": f"post {post_id}",
+                   "data": f"You are viewing the post with ID: {post_id}"})
 
 
-def profile_view(request, username):
+def profile_view(request: HttpRequest, username: str) -> HttpResponse:
+    """View function for profile page."""
     return render(request, "home/index.html",
-                  {"title": username, "data": f"You are viewing the user profile: {username}"})
+                  {"title": username,
+                   "data": f"You are viewing the user profile: {username}"})
 
 
-def event_view(request, year, month, day):
+def event_view(request: HttpRequest, year: int, month: int, day: int) -> HttpResponse:
+    """View function for event page."""
     return render(request, "home/index.html",
-                  {"title": f"{year}/{month}/{day}", "data": f"Event date: {year}-{month}-{day}"})
+                  {"title": f"{year}/{month}/{day}",
+                   "data": f"Event date: {year}-{month}-{day}"})
