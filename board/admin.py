a700838811f736ee078c18fc3ecdc93ada333516
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2026-05-04 02:15:11 +0300

    feat: add custom management command to seed database with Faker, extend models, customize admin

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,52 @@
+"""This module defines the configuration of the admin panel
+ and the registration of models."""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import CustomUser, Comment, Category, Ad
 
-# Register your models here.
+
+class CustomUserInline(admin.StackedInline):
+    """Inline representation of CustomUser model for UserAdmin."""
+    model = CustomUser
+
+
+class UserAdmin(BaseUserAdmin):
+    """Admin configuration for the User model."""
+    inlines = (CustomUserInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin configuration for the Category model."""
+    list_display = ("name", "get_count_active_ads")
+    readonly_fields = ("id", "get_count_active_ads")
+
+    list_filter = ("name",)
+    search_fields = ("name",)
+    search_help_text = "Search by category name"
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    """Admin configuration for the Ad model."""
+    list_display = ("title", "price", "category", "user", "is_active")
+    readonly_fields = ("id", "created_at", "updated_at")
+
+    list_filter = ("category", "is_active")
+    search_fields = ("title",)
+    search_help_text = "Search by ad title"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Admin configuration for the Comment model."""
+    list_display = ("content", "ad", "user", "created_at")
+    readonly_fields = ("id", "created_at",)
+
+    search_fields = ("content",)
+    search_help_text = "Search by comment content"
--- board/models.py
+++ board/models.py
@@ -1,9 +1,14 @@
 """Module containing model definitions for board application."""
+from datetime import timedelta
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
-class CustomUser(User):
+class CustomUser(models.Model):
     """Model definition for CustomUser based on Django's User model.
     CustomUser and Category have One-to-Many relationship.
 
@@ -12,8 +17,13 @@ class CustomUser(User):
             password: required;
             phone (CharField): required and unique;
             address (CharField): required."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=12, unique=True)
     address = models.CharField(max_length=50)
+
+    def __str__(self):
+        """Return username as a string representation of CustomUser class."""
+        return self.user.username
 
 
 class Category(models.Model):
@@ -25,6 +35,17 @@ class Category(models.Model):
             description (TextField): required."""
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        """Return name as a string representation of Category class."""
+        return self.name
+
+    def get_count_active_ads(self) -> int:
+        """Return count of active ads in the category."""
+        return self.ads.filter(is_active=True).count()
 
 
 class Ad(models.Model):
@@ -43,12 +64,28 @@ class Ad(models.Model):
             category (ForeignKey) references Category."""
     title = models.CharField(max_length=50)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0.00'))])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField()
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="ads")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="ads")
+
+    def __str__(self):
+        """Return title as a string representation of Ad class."""
+        return self.title
+
+    def get_short_description(self) -> str:
+        """Return the shortened version of the description (up to 100 characters)."""
+        if len(self.description) > 100:
+            return self.description[:100] + "..."
+        return self.description
+
+    def deactivate(self) -> None:
+        """Deactivate the active ad 30 days after it is created."""
+        if self.is_active and (timezone.now() - self.created_at) > timedelta(days=30):
+            self.is_active = False
+            self.save()
 
 
 class Comment(models.Model):
@@ -62,5 +99,9 @@ class Comment(models.Model):
             user (ForeignKey) references CustomUser;"""
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        """Return content as a string representation of Comment class."""
+        return self.content
